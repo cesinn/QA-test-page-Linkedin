2608817fa9ae4dfca5b273b83e4a4be4644abcb1
cesinn <cesarmarchi6@gmail.com>
2023-11-17 18:46:55 -0300

    teste commit

--- features/pages/home.page.rb
+++ features/pages/home.page.rb
@@ -1,17 +1,17 @@
-# class HomePage < SitePrism::page 
-#     element :userName, :xpath, "/html/body/div[4]/div[3]/div/div/div[2]/div/div/div/div/div[1]/div[1]/a/div[2]"
-#     element :navBarHome, :xpath, "/html/body/div[4]/header/div/nav/ul/li[1]/a/span"
-#     element :myIcon, :id, "ember15"
+class HomePage < SitePrism::page 
+    element :userName, :xpath, "/html/body/div[4]/div[3]/div/div/div[2]/div/div/div/div/div[1]/div[1]/a/div[2]"
+    element :navBarHome, :xpath, "/html/body/div[4]/header/div/nav/ul/li[1]/a/span"
+    element :myIcon, :id, "ember15"
     
-#     def checkLoginSuccessful
-#         expect(userName.text).to eql "César teste teste"
-#         expect(navBarHome.text).to eql "Início"
-#         expect(myIcon.text).to eql "Eu"
-#     end
+    def checkLoginSuccessful
+        expect(userName.text).to eql "César teste teste"
+        expect(navBarHome.text).to eql "Início"
+        expect(myIcon.text).to eql "Eu"
+    end
 
 
 
-# end
+end
 
 
 
